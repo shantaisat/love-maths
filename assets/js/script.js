dc71682a5ca4380c67c00f26f20b10f06b3a75e9
shantaisat <shanta.mahoneyis@gmail.com>
2025-01-28 19:23:11 +0000

    added event listeners to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+  // Code goes here
+});
 function runGame() {
   // Generate two random numbers
   
