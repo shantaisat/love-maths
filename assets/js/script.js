c3726aa0e2e8ec17098d2d567cab22a261a49939
shantaisat <shanta.mahoneyis@gmail.com>
2025-01-28 18:58:30 +0000

    added functions to js script

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+  // Generate two random numbers
+  
+}
+function checkAnswer() {
+  // Calculate the correct answer
+  
+}
+function calculateCorrectAnswer() {
+  // Return the correct answer
+  
+}
+function incrementScore() {
+  // Increment the score
+  
+}
+function incrementWrongAnswer() {
+  // Increment the incorrect answer
+  
+}
+function displayAdditionQuestion() {
+  // Display the question
+  
+}
+function displaySubtractQuestion() {
+  // Display the question
+  
+}
+function displayMultiplyQuestion() {
+  // Display the question
+  
+}
